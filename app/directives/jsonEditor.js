af363382d83d366b3b11c7e3c4ef91bc1e7678b9
Jason Parent <jason.parent@carneylabs.com>
2014-07-08 12:16:35 -0400

    Initial commit.

---
+++ app/directives/jsonEditor.js
@@ -0,0 +1,84 @@
+(function() {
+
+    var jsonEditorCtrl = function($rootScope) {
+        $rootScope.editor = {};
+
+        $rootScope.editor.update = function(content, caretPosition) {
+            if (content) {
+                $rootScope.editor.input = $rootScope.editor.input || "";
+
+                if (caretPosition) {
+                    var start = $rootScope.editor.input.substring(0, caretPosition);
+                    var end = $rootScope.editor.input.substr(caretPosition);
+
+                    $rootScope.editor.input = start + content + end;
+                }
+                else {
+                    $rootScope.editor.input = $rootScope.editor.input + content;
+                }
+            }
+        };
+
+        $rootScope.editor.validate = function() {
+            if ($rootScope.editor.input && $rootScope.editor.input.length > 0) {
+                try {
+                    $rootScope.editor.json = angular.fromJson($rootScope.editor.input);
+                }
+                catch (exception) {
+                    return false;
+                }
+            }
+
+            return true;
+        };
+
+        $rootScope.editor.validationClass = function() {
+            return $rootScope.editor.validate() ? "" : "has-error";
+        };
+
+        $rootScope.editor.clear = function() {
+            $rootScope.editor.input = "";
+        };
+
+        $rootScope.editor.setCaretPosition = function(event) {
+            if (event) {
+                $rootScope.editor.caretPosition = event.target.selectionStart ? event.target.selectionStart : 0;
+            }
+        };
+
+        $rootScope.editor.onClearButtonClick = function() {
+            $rootScope.editor.clear();
+        };
+
+        $rootScope.editor.onPrettifyButtonClick = function() {
+            if ($rootScope.editor.input && $rootScope.editor.validate()) {
+                var jsonInput = angular.fromJson($rootScope.editor.input);
+
+                $rootScope.editor.input = angular.toJson(jsonInput, true);
+            }
+        };
+
+        $rootScope.editor.onMinifyButtonClick = function() {
+            if ($rootScope.editor.input && $rootScope.editor.validate()) {
+                var jsonInput = angular.fromJson($rootScope.editor.input);
+
+                $rootScope.editor.input = angular.toJson(jsonInput, false);
+            }
+        };
+    };
+
+    jsonEditorCtrl.$inject = ["$rootScope"];
+
+    var jsonEditor = function() {
+        return {
+            restrict: "E",
+            replace: true,
+            controller: jsonEditorCtrl,
+            templateUrl: "/json-editor.html"
+        };
+    };
+
+    angular.module("jsonEditorApp", [])
+        .directive("jsonEditor", jsonEditor);
+
+}());
